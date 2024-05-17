30ff342717c96ba8a9a9cf0b1236a75728732c0c
Viniciusovski <vini98.2012@gmail.com>
2024-05-16 21:10:56 -0300

    Escopo de variáveis

---
+++ CFB_Course_CS/Aula04/Aula04.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula04{
+    static int num =10 // Variavel Global e estatica (para usar no metodo estatico)
+    static void Main(){
+        int num2 = 0;
+    }
+
+    void teste(){
+        Console.WriteLine(num); // Erro
+    }
+}
