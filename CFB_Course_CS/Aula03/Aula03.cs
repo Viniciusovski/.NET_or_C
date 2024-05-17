9ad091439246fc40661706ca680e113c3dbbbbe8
Viniciusovski <vini98.2012@gmail.com>
2024-05-16 21:01:14 -0300

    Variáveis

---
+++ CFB_Course_CS/Aula03/Aula03.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Aula03{
+    static void Main(){
+        byte n1 = 10 // 0 a 255
+        int num = -10;
+        char letra = 'c'
+        float valor = 5.3f;
+        string nome = "Vinicius";
+
+        var aux = 10; // O tipo é determinado na atribuição e declaração
+
+        // Ler variavel
+        Console.WriteLine(aux);
+        Console.WriteLine("Valor da variavel: " + aux + "...");
+        // Declaração de multiplas variaveis
+        int num1, num2, res;
+
+        num1 = 10;
+        num2 = 2;
+        res = num1 + num2;
+        Console.WriteLine("A soma de " + num1 + " mais " + num2 + " é igual a: " + res);
+        Console.WriteLine("A soma de " + num1 + " mais " + num2 + " é igual a: " + num1 + num2);
+        
+    }
+}
