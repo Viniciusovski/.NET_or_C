2be2c3be9c4eb813580bb311076e1c04f46adea3
Viniciusovski <vini98.2012@gmail.com>
2024-05-20 21:41:36 -0300

    Loop FOREACH / Estruturas de iteração

---
+++ CFB_Course_CS/Aula22/Aula22.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Aula22{
+
+    static void Main(){
+        int[] num = new int[3]{11,22,33,44,55};
+
+        // for(int i = 0; i<num.Length; i++){
+        //     Console.WriteLine(num[i]);
+        // }
+
+        for(int i = 0; i<num.Length; i++){
+            num[i]=0;
+        }
+
+        foreach(int n in num){
+            Console.WriteLine(n);
+        }
+        // Não é possível atribuir valaores no array com o foreach
+
+    }
+}
